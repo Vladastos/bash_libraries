a30b95d6df05a85d2da7a3d082c1f0b16094663e
Vladislav Parfeniuc <vladislavparfeniuc@gmail.com>
2024-04-21 16:11:53 +0200

    first attempt at caching

--- main.bash
+++ main.bash
@@ -1,16 +1,33 @@
 #!/bin/bash
-vladastos_libs(){
+check_if_in_cache_and_execute(){
+    #TODO check if library is already in cache and load from there
+    local CACHE_DIR="$HOME"/.cache/vlibs
+    mkdir -p "$CACHE_DIR"
     local lib_name="$1"
-    echo "Using version 0.0.3"
-    echo "Loading library: $lib_name"
     shift
-    local lib_path=https://raw.githubusercontent.com/Vladastos/bash_libraries/main/lib/$lib_name.bash
-    if ! wget -q --spider "$lib_path" &>/dev/null; then
-        echo "Library $lib_name not found"
+    local lib_remote_path=https://raw.githubusercontent.com/Vladastos/bash_libraries/main/lib/$lib_name.bash
+    local lib_path="$CACHE_DIR"/"$lib_name".bash
+    if [ ! -f "$lib_path" ]; then
+        #if not in cache download
+        if ! wget -q --spider "$lib_remote_path" &>/dev/null; then
+            echo "Library $lib_name not found"
             return 1
+        fi
+        echo "Caching library: $lib_name"
+        touch "$lib_path"
+        wget -qO "$lib_path" "$lib_remote_path"
     fi
-    local lib=$(wget -qO- "$lib_path")
-    source <(echo "$lib")
+    echo "Executing library: $lib_name"
+    #shellcheck source=/dev/null
+    source "$lib_path"
     "$lib_name" "$@"
+
+}
+vladastos_libs(){
+    if [ "$#" -lt 0 ]; then
+        echo "Usage: vladastos_libs <lib_name>"
+    fi
+    echo "Using version 0.0.5"
+    check_if_in_cache_and_execute "$@"
 }
 
